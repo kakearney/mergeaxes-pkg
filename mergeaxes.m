104a837082c017c47fd8b0da6cc1284da49e317c
Kelly Kearney <kakearney@gmail.com>
2014-11-26 15:16:50 -0500

    Finalizing reorganization of files

---
+++ mergeaxes.m
@@ -0,0 +1,32 @@
+function newax = mergeaxes(ax)
+%MERGEAXES Create new axis that occupies space of previous ones
+%
+% newax = mergeaxes(ax)
+%
+% Input variables:
+%
+%   ax:     array of axis handles.  These objects will be deleted.
+%
+% Output variables:
+%
+%   newax:  handle of new axis, locating the combined space of the input
+%           axes.
+
+% Copyright 2013 Kelly Kearney
+
+pos = arrayfun(@(x) getpos(x, 'nz'), ax, 'uni', 0);
+pos = cat(1, pos{:});
+
+l = min(pos(:,1));
+r = max(pos(:,1) + pos(:,3));
+b = min(pos(:,2));
+t = max(pos(:,2) + pos(:,4));
+
+newpos = [l b r-l t-b];
+
+delete(ax);
+newax = axes('position', newpos);
+
+
+
+
